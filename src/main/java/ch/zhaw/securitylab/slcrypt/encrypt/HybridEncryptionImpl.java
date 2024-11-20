619780929f9c4da89c5dcd9de8825453167b6075
Timo <timo.sigrist@gmail.com>
2024-11-20 22:28:22 +0100

    Lab 7

--- src/main/java/ch/zhaw/securitylab/slcrypt/encrypt/HybridEncryptionImpl.java
+++ src/main/java/ch/zhaw/securitylab/slcrypt/encrypt/HybridEncryptionImpl.java
@@ -1,116 +1,196 @@
 package ch.zhaw.securitylab.slcrypt.encrypt;
 
 import java.io.InputStream;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.ChaCha20ParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import ch.zhaw.securitylab.slcrypt.FileHeader;
+import ch.zhaw.securitylab.slcrypt.Helpers;
+import java.io.ByteArrayOutputStream;
+import java.security.spec.PKCS8EncodedKeySpec;
 
-/**
- * A concrete implementation of the abstract class HybridEncryption.
- */
 public class HybridEncryptionImpl extends HybridEncryption {
 
-    /**
-     * Creates a secret key.
-     *
-     * @param cipherAlgorithm The cipher algorithm to use
-     * @param keyLength The key length in bits
-     * @return The secret key
-     */
     @Override
     protected byte[] generateSecretKey(String cipherAlgorithm, int keyLength) {
-
-        // To do...
-        return null;
+        try {
+            String cipherName = Helpers.getCipherName(cipherAlgorithm);
+            
+            if (cipherName.equals("SEED")) {
+                byte[] key = new byte[16];
+                SecureRandom random = new SecureRandom();
+                random.nextBytes(key);
+                return key;
+            }
+            
+            KeyGenerator keyGen = KeyGenerator.getInstance(cipherName);
+            keyGen.init(keyLength);
+            SecretKey secretKey = keyGen.generateKey();
+            return secretKey.getEncoded();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error generating secret key", e);
+        }
     }
 
-    /**
-     * Encrypts the secret key with a public key.
-     *
-     * @param secretKey The secret key to encrypt
-     * @param certificateEncrypt An input stream from which the certificate with
-     *                           the public key for encryption can be read
-     * @return The encrypted secret key
-     */
     @Override
-    protected byte[] encryptSecretKey(byte[] secretKey, 
-            InputStream certificateEncrypt) {
+    protected byte[] encryptSecretKey(byte[] secretKey, InputStream certificateEncrypt) {
+        try {
+            CertificateFactory factory = CertificateFactory.getInstance("X.509");
+            Certificate certificate = factory.generateCertificate(certificateEncrypt);
+            PublicKey publicKey = certificate.getPublicKey();
 
-        // To do...
-        return null;
+            Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPPadding");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+            return cipher.doFinal(secretKey);
+        } catch (Exception e) {
+            throw new RuntimeException("Error encrypting secret key", e);
+        }
     }
 
-    /**
-     * Creates a file header object and fills it with the cipher algorithm name,
-     * the IV (which must first be created), the authentication and integrity 
-     * protection type and algorithm name, the certificate, and the encrypted 
-     * secret key.
-     *
-     * @param cipherAlgorithm The cipher algorithm to use
-     * @param authIntType The type to use for authentication and integrity
-     *                    protection (M for MAC, S for signature, N for none)
-     * @param authIntAlgorithm The algorithm to use for authentication and
-     *                         integrity protection
-     * @param certificateVerify An input stream from which the certificate for
-     *                          signature verification can be read
-     * @param encryptedSecretKey The encrypted secret key
-     * @return The new file header object
-     */
     @Override
-    protected FileHeader generateFileHeader(String cipherAlgorithm, 
-            char authIntType, String authIntAlgorithm, 
-            InputStream certificateVerify, byte[] encryptedSecretKey) {
-
-        // To do...
-        return null;
-    }
-
-    /**
-     * Encrypts a document with a secret key. If GCM is used, the file header is
-     * added as additionally encrypted data.
-     *
-     * @param document The document to encrypt
-     * @param fileHeader The file header that contains information for
-     * encryption
-     * @param secretKey The secret key used for encryption
-     * @return A byte array that contains the encrypted document
-     */
-    @Override
-    protected byte[] encryptDocument(InputStream document, 
-            FileHeader fileHeader, byte[] secretKey) {
-
-        // To do...
-        return null;
-    }
-
-    /**
-     * Computes the HMAC over a byte array.
-     *
-     * @param dataToProtect The input over which to compute the MAC
-     * @param macAlgorithm The MAC algorithm to use
-     * @param password The password to use for the MAC
-     * @return The byte array that contains the MAC
-     */
-    @Override
-    protected byte[] computeMAC(byte[] dataToProtect, String macAlgorithm, 
-            byte[] password) {
-
-        // To do...
-        return null;
-    }
-    
-    /**
-     * Computes the signature over a byte array.
-     *
-     * @param dataToProtect The input over which to compute the signature
-     * @param signatureAlgorithm The signature algorithm to use
-     * @param privateKeySign An input stream from which the private key to sign
-     *                       can be read
-     * @return The byte array that contains the signature
-     */
-    @Override
-    protected byte[] computeSignature(byte[] dataToProtect, 
-            String signatureAlgorithm, InputStream privateKeySign) {
+protected FileHeader generateFileHeader(String cipherAlgorithm, char authIntType, 
+                                        String authIntAlgorithm, InputStream certificateVerify, 
+                                        byte[] encryptedSecretKey) {
+    try {
         
-        // To do...
-        return null;
+        FileHeader header = new FileHeader();
+        header.setCipherAlgorithm(cipherAlgorithm);
+
+        // Generate and set IV, or set to an empty byte array if IV is not required
+        byte[] iv;
+        if (Helpers.hasIV(cipherAlgorithm)) {
+            iv = new byte[Helpers.getIVLength(cipherAlgorithm)];
+            SecureRandom random = new SecureRandom();
+            random.nextBytes(iv);
+        } else {
+            iv = new byte[0]; // No IV required
+        }
+        header.setIV(iv);
+        
+        header.setAuthIntType(authIntType);
+        header.setAuthIntAlgorithm(authIntAlgorithm);
+       
+        // read cert
+        byte[] certificateBytes;
+        if (certificateVerify != null && authIntType == Helpers.SIGNATURE) {
+            ByteArrayOutputStream certByteOS = new ByteArrayOutputStream();
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            while ((bytesRead = certificateVerify.read(buffer)) != -1) {
+                certByteOS.write(buffer, 0, bytesRead);  
+            }
+            certificateBytes = certByteOS.toByteArray();
+        } else {
+            certificateBytes = new byte[0]; 
+        }
+        header.setCertificate(certificateBytes);
+
+        header.setEncryptedSecretKey(encryptedSecretKey);
+
+        return header;
+    } catch (Exception e) {
+        throw new RuntimeException("Error generating file header", e);
     }
+}
+
+
+    @Override
+    protected byte[] encryptDocument(InputStream document, FileHeader fileHeader, byte[] secretKey) {
+        try {
+            String cipherAlgorithm = fileHeader.getCipherAlgorithm();
+            Cipher cipher = Cipher.getInstance(cipherAlgorithm);
+            
+            SecretKeySpec keySpec = new SecretKeySpec(secretKey, Helpers.getCipherName(cipherAlgorithm));
+            byte[] iv = fileHeader.getIV();
+            
+            if (Helpers.isCBC(cipherAlgorithm)){
+                IvParameterSpec ivSpec = new IvParameterSpec(iv);
+                cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+                
+            } else if (Helpers.isCTR(cipherAlgorithm)) {
+                IvParameterSpec ivSpec = new IvParameterSpec(iv);
+                cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+
+            } else if (Helpers.isGCM(cipherAlgorithm)) {
+                GCMParameterSpec gcmSpec = new GCMParameterSpec(Helpers.AUTH_TAG_LENGTH, iv);
+                cipher.init(Cipher.ENCRYPT_MODE, keySpec, gcmSpec);
+                cipher.updateAAD(fileHeader.encode()); // Add file header as AAD
+                
+            } else if (Helpers.isCHACHA20(cipherAlgorithm)) {
+                ChaCha20ParameterSpec chaChaSpec = new ChaCha20ParameterSpec(iv, 1);
+                cipher.init(Cipher.ENCRYPT_MODE, keySpec, chaChaSpec); // Counter = 1
+                
+            } else if (Helpers.hasIV(cipherAlgorithm)){
+                IvParameterSpec ivSpec = new IvParameterSpec(iv);
+                cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+                
+            } else {
+                cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+            }
+
+            ByteArrayOutputStream encryptedOutput = new ByteArrayOutputStream();
+
+            // Read data from CipherInputStream and write to ByteArrayOutputStream
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = document.read(buffer)) != -1) {
+                byte[] output = cipher.update(buffer, 0, bytesRead);
+                if (output != null) {
+                    encryptedOutput.write(output);
+                }                
+            }
+            byte[] outputFinal = cipher.doFinal();
+            if (outputFinal != null) {
+                encryptedOutput.write(outputFinal);
+            }
+            return encryptedOutput.toByteArray();
+            
+        } catch (Exception e) {
+            throw new RuntimeException("Error encrypting document", e);
+        }
+    }
+
+
+    @Override
+    protected byte[] computeMAC(byte[] dataToProtect, String macAlgorithm, byte[] password) {
+        try {
+          
+            SecretKeySpec keySpec = new SecretKeySpec(password, macAlgorithm);
+            Mac mac = Mac.getInstance(macAlgorithm);
+            mac.init(keySpec);
+
+            return mac.doFinal(dataToProtect);
+        } catch (Exception e) {
+            throw new RuntimeException("Error computing MAC", e);
+        }
+    }
+
+
+    @Override
+    protected byte[] computeSignature(byte[] dataToProtect, String signatureAlgorithm, InputStream privateKeySign) {
+        try {
+            byte[] privateKeyBytes = Helpers.inputStreamToByteArray(privateKeySign); // Use helper method
+            if (privateKeyBytes == null) {
+                throw new RuntimeException("Failed to convert InputStream to byte array");
+            }
+
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PrivateKey privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(privateKeyBytes));
+
+            Signature signature = Signature.getInstance(signatureAlgorithm);
+            signature.initSign(privateKey);
+            signature.update(dataToProtect);
+
+            return signature.sign();
+        } catch (Exception e) {
+            throw new RuntimeException("Error computing signature", e);
+        }
+    }
+
 }
